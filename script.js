279008443a913fb23bf71f608f6c93f63e27aa11
JetLiTheQT <ekkachai.ittihrit2@gmail.com>
2023-11-28 14:56:28 -0800

    testing index preloading background image

--- script.js
+++ script.js
@@ -18,15 +18,22 @@ document.addEventListener("DOMContentLoaded", function() {
 document.addEventListener('DOMContentLoaded', () => {
     const savedBackground = localStorage.getItem('backgroundImage');
     if (savedBackground) {
-        const preloadLink = document.createElement('link');
-        preloadLink.rel = 'preload';
-        preloadLink.href = savedBackground;
-        preloadLink.as = 'image';
-
-        document.head.appendChild(preloadLink);
+        const preloadStyle = document.getElementById('preloadStyle');
+        preloadStyle.textContent = `
+            body::before {
+                content: '';
+                display: block;
+                position: fixed;
+                top: -100%;
+                width: 1px;
+                height: 1px;
+                background-image: url('${savedBackground}');
+            }
+        `;
         document.body.style.backgroundImage = `url('${savedBackground}')`;
     }
 });
+
 
 function fetchAndDisplayQuote() {
     // Adding a unique timestamp to the URL to prevent caching
