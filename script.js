efa7ef4c934722d3de558b05aaaf417ab45b960d
JetLiTheQT <ekkachai.ittihrit2@gmail.com>
2023-11-28 17:28:29 -0800

    tryna fix

--- script.js
+++ script.js
@@ -72,6 +72,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const savedBackground = localStorage.getItem('backgroundImage');
     if (savedBackground) {
         document.body.style.backgroundImage = `url('${savedBackground}')`;
+        document.body.style.backgroundSize = 'cover'; // Set background size to cover
     }
 });
 
