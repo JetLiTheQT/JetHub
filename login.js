d7b590bb5c672176c4aba3357b4072f2432b0d43
JetLiTheQT <ekkachai.ittihrit2@gmail.com>
2023-12-04 17:06:20 -0800

    fixing

--- login.js
+++ login.js
@@ -1,4 +1,7 @@
-
+// login.js
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js';
 const firebaseConfig = {
     apiKey: "AIzaSyDrHw4QkFNEnNw2QVsMkVPOg4TtxbeQVwM",
     authDomain: "jethub-7e600.firebaseapp.com",
@@ -7,19 +10,20 @@ const firebaseConfig = {
     messagingSenderId: "723758012207",
     appId: "1:723758012207:web:40ea1ea84e9a2f460fb3da"
 };
+
+
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 const loginButton = document.getElementById('loginButton');
 
-// Listen for clicks on the login button
 loginButton.addEventListener('click', () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider)
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
         .then((result) => {
-            // User is signed in, you can handle the success here
+            // User is signed in
         })
         .catch((error) => {
             console.error('Google Sign-In Error:', error);
