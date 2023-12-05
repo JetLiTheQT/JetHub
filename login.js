e2216946b87c6f7a1a46e2e644712349f6354505
JetLiTheQT <ekkachai.ittihrit2@gmail.com>
2023-12-04 16:59:16 -0800

    ah

--- login.js
+++ login.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app);s
+const db = getFirestore(app);
 
 const loginButton = document.getElementById('loginButton');
 
